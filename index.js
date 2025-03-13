d7e7b01194ff08fc1efbc0b011f26312ef2bacc3
Repr <reprdev1@gmail.com>
2025-03-13 06:40:06 -0400

    Initialized npm, added dependency, and made minor edits to index.js

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const prompt = require("prompt-sync")();
 
 // NodeJS experience
 console.log("\nWelcome!");
-console.log("Type \"q\" to quit at any time.\n");
+console.log('Type "q" to quit at any time.\n');
 
 while (true) {
     let res = prompt("Number to flip > ");
 
-    if(res === "q") break;
+    if (res === "q") break;
 
-    console.log(`${turnCalc(res)}\n`);
+    let flipped = turnCalc(res);
+
+    if(flipped === "llun") break; // Allows user to exit with [ctrl+C]
+    else console.log(`${flipped}\n`);
 }
 
 /**
- * 
- * @param {Number, String} num - input value (string is recommended so that numbers with leading zeros can be inputted)
- * @returns {String} flipped number
- *  
+ * The number flipping function
+ *
+ * @param {(number|string)} num - input value (string is recommended so that numbers with leading zeros can be inputted)
+ * @returns {string} flipped number
+ *
  */
 function turnCalc(num) {
     // Finds the stringified input, but reversed
